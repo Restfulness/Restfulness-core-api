c4b647155e790f2cb4e9494a6e3af81d1dd9b4e1
Farbod <farbodahmadian2014@gmail.com>
2020-08-17 15:44:28 +0430

    Added test (Pytest) to project

---
+++ tests/test_login.py
@@ -0,0 +1,32 @@
+# This module tests to see login API works correctly or not
+import json
+
+# curl -i -H "Content-Type: application/json" -X POST 
+# -d '{"username": "user1", "password": "zanjan"}' localhost:5000/login
+def test_login_successful(app, client):
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    data = {
+        "username": "user1",
+        "password": "zanjan"
+    }
+    res = client.post("/login", data=json.dumps(data), headers=headers)
+    #res = client.get('/')
+    assert res.status_code == 200
+    #expected = {'hello': 'world'}
+    #assert expected == json.loads(res.get_data(as_text=True))
+
+
+# curl -i -H "Content-Type: application/json" -X POST 
+# -d '{"username": "xxxx", "password": "yyyy"}' localhost:5000/login
+def test_login_failed(app, client):
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    data = {
+        "username": "xxxx",
+        "password": "yyyy"
+    }
+    res = client.post("/login", data=json.dumps(data), headers=headers)
+    assert res.status_code == 401
