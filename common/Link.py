f2a3994d0d522e252733ffaefa95ee89d64fe11f
Farbod <farbodahmadian2014@gmail.com>
2020-08-22 15:57:25 +0430

    Make list validation more clearer

--- common/Link.py
+++ common/Link.py
@@ -1,12 +1,11 @@
 class Link():
     def __init__(self, address_name, categories=""):
         self.address_name = address_name
-        self.categories = []
         # Make sure that input is a list
-        if not isinstance(categories, list):
-            self.categories.append(categories)
-        else:
+        if isinstance(categories, list):
             self.categories = categories
+        else:
+            self.categories = [categories]
 
     def get_categories(self):
         return self.categories
