221a583f05bf721c81fb51c91e5b1e8f65a06474
Farbod <farbodahmadian2014@gmail.com>
2020-11-17 20:49:54 +0330

    Generate hash of user's ID and valid 8 digit code

--- common/ResetPasswordCore.py
+++ common/ResetPasswordCore.py
@@ -1,5 +1,8 @@
 from common.DbHandler import DbHandler
 
+from itsdangerous import (TimedJSONWebSignatureSerializer
+                          as Serializer, BadSignature, SignatureExpired)
+
 import random
 import string
 
@@ -15,11 +18,27 @@ class ResetPasswordCore:
         random_code = ResetPasswordCore.__generate_8_digit_code()
         ResetPasswordCore.__send_8_digit_code_to_users_email(
             username, random_code)
-        # TODO: Store 8-digit password
+
+        return(
+            ResetPasswordCore.__generate_hash_string(id, random_code)
+        )
+
+    @staticmethod
+    def __generate_hash_string(id: int, random_code: str) -> str:
+        """ Create a hash that contains (user's ID and valid
+        random created 8 digit code) which expires in 300 seconds."""
+        hash = Serializer('test', expires_in=300)
+        return(
+            hash.dumps({
+                'id': id,
+                'valid_code': random_code
+            })
+        )
 
     @staticmethod
     def __send_8_digit_code_to_users_email(username: str, random_code: str):
         """ Send random generated password to user's email."""
+        # TODO: Implement sending email part
         pass
 
     @staticmethod
